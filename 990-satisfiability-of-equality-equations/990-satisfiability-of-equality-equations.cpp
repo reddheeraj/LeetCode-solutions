e19abed3dc3be81d5a209242735cb8f5d10d7d3d
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2022-09-26 23:13:38 +0530

    Time: 23 ms (8.74%), Space: 11.5 MB (33.76%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    unordered_map<char,char> par;
+public:
+    char parent(char c){
+        if(par[c]==c) return c;
+        return par[c]=parent(par[c]);
+    }
+	
+    void unite(char a,char b){
+        par[a]=b;
+    }
+    
+    bool equationsPossible(vector<string>& equations) {
+        for(int i=0;i<26;i++) par['a'+i] = i+'a';
+		
+        for(auto &eqn : equations){
+            char a = eqn[0];
+            char b = eqn[3];
+            
+            if(eqn[1] == '='){
+                char parA = parent(a);
+                char parB = parent(b);
+				
+                if(parA != parB)
+					unite(parA,parB);
+            }
+        }		
+        for(auto &eqn : equations){
+            char a = eqn[0];
+            char b = eqn[3];
+            
+            if(eqn[1] == '!'){
+                char parA = parent(a);
+                char parB = parent(b);
+				
+                if(parA == parB)
+					return false;
+            }
+        }		
+        return true;
+    }
+};
