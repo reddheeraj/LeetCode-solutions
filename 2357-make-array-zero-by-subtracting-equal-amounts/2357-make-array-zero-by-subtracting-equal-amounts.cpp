448debf1b52d3405bcb0a94972c016996207fd23
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2022-10-19 14:52:03 +0530

    Time: 141 ms (7.47%), Space: 8.3 MB (84.01%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,30 @@
+int SUM(vector<int>& nums) {
+    int sum = 0;
+    for (auto &n: nums) 
+        sum += n;
+    return sum;
+}
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int ans = 0;
+        while(SUM(nums)) {
+            int small = 9999;
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] < small && nums[i] != 0) small = nums[i];
+            }
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0) {
+                    nums[i] = nums[i] - small;   
+                }
+            }
+            for (auto &i: nums) { 
+                cout<<i<<" ";
+            }
+            cout<<"\n";
+            ans++;
+        }
+        return ans;
+    }
+};
