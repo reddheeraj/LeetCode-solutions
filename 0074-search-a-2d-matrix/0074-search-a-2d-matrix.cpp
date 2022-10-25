69379a448265f9015c2cc05a85c2acfe3fc01cf1
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2022-10-25 14:05:38 +0530

    Time: 14 ms (12.51%), Space: 9.5 MB (51.47%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,12 +1,10 @@
 #pragma GCC optimize('O2')
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size();
-        int cols = matrix[0].size(); 
-        int row = 0, col = cols - 1;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) { 
+        int row = 0, col = matrix[0].size() - 1;
 			
-        while (row < rows && col > -1) {
+        while (row < matrix.size() && col > -1) {
             int cur = matrix[row][col];
             if (cur == target) return true;
             if (target > cur) row++;
