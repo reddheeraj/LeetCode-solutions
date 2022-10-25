960913fbf231a4b221da1161b869299eea3fa64e
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2022-10-25 14:04:05 +0530

    Time: 8 ms (53.09%), Space: 9.5 MB (51.47%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,12 +2,6 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // for(int i = 0; i < matrix.size(); i++) {  //brute force approach
-        //     for (int j = 0; j < matrix[0].size(); j++) {
-        //         if (target == matrix[i][j]) return true;
-        //     }
-        // }
-        //return false;
         int rows = matrix.size();
         int cols = matrix[0].size(); 
         int row = 0, col = cols - 1;
