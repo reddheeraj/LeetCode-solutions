c96fd7153885f80131c85c77320a33e83cd37c4b
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2023-03-24 22:06:07 +0530

    Added solution - LeetHub

--- Preorder Traversal - GFG/preorder-traversal.cpp
+++ Preorder Traversal - GFG/preorder-traversal.cpp
@@ -129,19 +129,23 @@ struct Node
 */
 
 //Function to return a list containing the preorder traversal of the tree.
-void pretrav(Node* root, vector<int> &v) {
-    if (!root) return;
-    
-    v.push_back(root->data);
-    pretrav(root->left, v);
-    pretrav(root->right, v);
 
-}
 
 vector <int> preorder(Node* root)
 {
   // Your code here
+  
   vector<int> v;
-  pretrav(root, v);
+  stack<Node*> s;
+  if (root == NULL) return v;
+  s.push(root);
+  while(!s.empty()) {
+      Node* temp = s.top();
+      s.pop();
+      v.push_back(temp->data);
+      if(temp->right != NULL) s.push(temp->right);
+      if(temp->left != NULL) s.push(temp->left);
+  }
+  
   return v;
 }
