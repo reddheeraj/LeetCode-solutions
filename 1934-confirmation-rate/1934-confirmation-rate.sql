e9fb1171a7803eccb89e4aaa4ee21c842ab22119
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2024-11-03 18:39:56 -0600

    Time: 698 ms (66.79%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select distinct s.user_id, 
+round((sum(case when action = 'confirmed' then 1 else 0 end) over(partition by s.user_id))/(count(*) over (partition by s.user_id)), 2)
+as confirmation_rate
+from Signups as s
+left join Confirmations as c
+on s.user_id = c.user_id
+order by confirmation_rate;
