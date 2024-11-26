4ed81e1bb1dfea70e4c89e248ae4432b7043c677
Dheeraj Reddy <77443480+reddheeraj@users.noreply.github.com>
2024-11-26 11:44:13 -0600

    Time: 1233 ms (34.53%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+-- select s.product_id, min(year) as first_year, quantity, price
+-- from Sales s
+-- INNER JOIN Product p
+-- where s.product_id = p.product_id
+-- group by s.product_id;
+
+with ranks as (
+    select *, rank() over (partition by product_id order by year) as rank_order
+    from Sales
+)
+select r.product_id, year as first_year, quantity, price
+from ranks r
+INNER JOIN Product p
+on r.product_id = p.product_id
+where rank_order = 1;
