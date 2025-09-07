ef0fd05bbbf2f4f45f69839e0532b97f1b074221
Dheeraj Mudireddy <77443480+reddheeraj@users.noreply.github.com>
2025-09-07 17:00:00 -0500

    Time: 0 ms, Memory: 17.7 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -1,12 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = ""
+        num = 0
+        r = 1
+        i = len(digits) - 1
+        while i >= 0:
+            num = num + digits[i] * r
+            r *= 10
+            i -= 1
+        num += 1
+        # print(num)
         res = []
-        for i in digits:
-            num += str(i)
-        num = int(num) + 1
         while num > 0:
             res.append(num % 10)
-            num = num//10
-        return res[::-1]
+            num = num // 10
         
+        res.reverse()
+        return res
+            
