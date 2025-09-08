bd6a6cb5f60dbbf879ea51e8d4e0487c6541b8cc
Dheeraj Mudireddy <77443480+reddheeraj@users.noreply.github.com>
2025-09-08 16:32:35 -0500

    Time: 0 ms, Memory: 19.1 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,18 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         res = {}
         for i in range(len(nums)):
-            if target - nums[i] in list(res.keys()):
-                # print("nums[i] = ", nums[i])
-                # print("target - nums[i]= ", target - nums[i])
-                return res[target-nums[i]], i
-                # return res[nums[i]], res[target-nums[i]]
-                  
+            if target-nums[i] in res:
+                return [i, res[target-nums[i]]]
+                # check in res and return indices
+            
             res[nums[i]] = i
-        print(res)    
-        
