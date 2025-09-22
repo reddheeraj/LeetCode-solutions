f0c5faa0d2294f8ed7f0c15d976d90ffc1138330
Dheeraj Mudireddy <77443480+reddheeraj@users.noreply.github.com>
2025-09-22 08:23:52 -0500

    Time: 71 ms, Memory: 18.1 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,9 +1,16 @@
-from collections import Counter
+from collections import OrderedDict
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        res = Counter(s)
-        
+        c = OrderedDict()
         for i in range(len(s)):
-            if res[s[i]] == 1:
-                return i
+            if s[i] not in c:
+                c[s[i]] = [i, 1]
+            else:
+                c[s[i]][1] += 1
+        
+        # print(c)
+        for k,v in c.items():
+            if v[1] == 1:
+                return v[0]
+            
         return -1
